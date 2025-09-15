dc4aebcfe0717f259958d4b9b05dbe8221ec2299
Momen Mohmoud <moka.32f@gmail.com>
2025-09-15 19:55:57 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "clsLoginScreen.h" 
+#include "Global.h"
+
+int main(int argc, char *argv[])
+{
+	while (clsLoginScreen:: ShowLoginScreen()){
+	    
+	     
+	      }; 
+}
